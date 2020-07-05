c3fa237cadaef26caa59448bec52499e62f92941
kouki4416 <pank4416@gmail.com>
2020-07-06 04:45:53 +0900

    [Add]Added bone structure of block chain

---
+++ src/blockChain/proof.go
@@ -0,0 +1,93 @@
+package blockChain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+//several part of consensus algorithms: this time proof of work(other is proof of stake)
+
+//Take the data from the block
+
+//create a counter which starts at 0
+
+//create a hash of the data plus the counter
+
+//check the hash to see if it meets a set of requirement
+
+//Requirements:
+//The first few bytes must contain 0s
+
+const Difficulty = 20//arbitrary num, this increments as time past
+
+type ProofOfWork struct{
+	Block *Block
+	Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork{
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))//left shift target
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+			[][]byte{
+				pow.Block.PrevHash,
+				pow.Block.Data,
+				ToHex(int64(nonce)),
+				ToHex(int64(Difficulty)),
+			},
+			[]byte{},
+		)
+	return data
+}
+
+func (pow *ProofOfWork) Validate() bool{
+	var intHash big.Int
+
+	data:= pow.InitData(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
+
+func (pow *ProofOfWork) Run() (int, []byte){
+	var intHash big.Int
+	var hash [32]byte
+
+	nonce := 0
+
+	for nonce < math.MaxInt64{
+		data := pow.InitData(nonce)
+		hash = sha256.Sum256(data)
+
+		fmt.Printf("\r%x", hash)
+		intHash.SetBytes(hash[:])
+
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		}else{
+			nonce++
+		}
+	}
+	fmt.Println()
+	return nonce, hash[:]
+}
+
+func ToHex(num int64) []byte {
+	buff := new(bytes.Buffer)
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil{
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
